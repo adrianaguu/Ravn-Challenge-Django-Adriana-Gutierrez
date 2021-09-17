3e27fec39ceda5cf671e4299118cb87527269048
adrianaguu <adriana.fabiola.gutierrez@gmail.com>
2021-09-17 15:05:00 -0500

    feat: models created and registered in admin site

---
+++ people/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Species(models.Model):
+    name = models.CharField(max_length=50,help_text="The name of this species.")
+
+    def __str__(self):
+        return self.name
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=50,help_text="The name of this planet.")
+
+    def __str__(self):
+        return self.name
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=50,help_text="The name of this vehicle.")
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=50,help_text="The name of this person.")
+
+    birth_year = models.CharField(max_length=10,help_text="""The birth year of the person, 
+    using the in-universe standard of BBY or ABY - Before the Battle of Yavin or After the Battle of Yavin. 
+    The Battle of Yavin is a battle that occurs at the end of Star Wars episode IV: A New Hope.""")
+
+    eye_color = models.CharField(max_length=10,help_text="""The eye color of this person. Will be \"unknown\" 
+    if not known or \"n/a\" if the person does not have an eye.""")
+
+    hair_color = models.CharField(max_length=10,help_text="""The hair color of this person. Will be \"unknown\" 
+    if not known or \"n/a\" if the person does not have hair.""")
+
+    skin_color = models.CharField(max_length=10,help_text="The skin color of this person.")
+
+    homeworld = models.ForeignKey(Planet, on_delete=models.PROTECT)
+
+    species = models.ManyToManyField(Species,help_text="Set of species that this person belongs to.")
+
+    vehicles = models.ManyToManyField(Vehicle,help_text="Set of vehicles that this person has piloted.")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "People"
