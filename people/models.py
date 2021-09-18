64de58ec72b50b57c9c474cbf2705df16926ae95
adrianaguu <adriana.fabiola.gutierrez@gmail.com>
2021-09-17 22:27:59 -0500

    data: seed data for 20 people

--- people/models.py
+++ people/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 class Species(models.Model):
-    name = models.CharField(max_length=50,help_text="The name of this species.")
+    name = models.CharField(max_length=50,help_text="The name of this species.",unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Planet(models.Model):
-    name = models.CharField(max_length=50,help_text="The name of this planet.")
+    name = models.CharField(max_length=50,help_text="The name of this planet.",unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Vehicle(models.Model):
-    name = models.CharField(max_length=50,help_text="The name of this vehicle.")
+    name = models.CharField(max_length=50,help_text="The name of this vehicle.",unique=True)
 
     def __str__(self):
         return self.name
@@ -23,25 +23,25 @@ class Vehicle(models.Model):
 
 class Person(models.Model):
 
-    name = models.CharField(max_length=50,help_text="The name of this person.")
+    name = models.CharField(max_length=50,help_text="The name of this person.",unique=True)
 
     birth_year = models.CharField(max_length=10,help_text="""The birth year of the person, 
     using the in-universe standard of BBY or ABY - Before the Battle of Yavin or After the Battle of Yavin. 
     The Battle of Yavin is a battle that occurs at the end of Star Wars episode IV: A New Hope.""")
 
-    eye_color = models.CharField(max_length=10,help_text="""The eye color of this person. Will be \"unknown\" 
+    eye_color = models.CharField(max_length=20,help_text="""The eye color of this person. Should be \"unknown\" 
     if not known or \"n/a\" if the person does not have an eye.""")
 
-    hair_color = models.CharField(max_length=10,help_text="""The hair color of this person. Will be \"unknown\" 
+    hair_color = models.CharField(max_length=20,help_text="""The hair color of this person. Should be \"unknown\" 
     if not known or \"n/a\" if the person does not have hair.""")
 
-    skin_color = models.CharField(max_length=10,help_text="The skin color of this person.")
+    skin_color = models.CharField(max_length=20,help_text="The skin color of this person.")
 
-    homeworld = models.ForeignKey(Planet, on_delete=models.PROTECT)
+    homeworld = models.ForeignKey(Planet, on_delete=models.PROTECT,help_text="Planet that this person was born on or inhabits.")
 
     species = models.ManyToManyField(Species,help_text="Set of species that this person belongs to.")
 
-    vehicles = models.ManyToManyField(Vehicle,help_text="Set of vehicles that this person has piloted.")
+    vehicles = models.ManyToManyField(Vehicle,help_text="Set of vehicles that this person has piloted.",null=True,blank=True)
 
     def __str__(self):
         return self.name
